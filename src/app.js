111b19ffd8b2268339db27ba5daae5ac8303c093
Ángel Sánchez Moreno <univ.angel97@gmail.com>
2025-01-13 12:13:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let t = 0; t < adj.length; t++) {
+      for (let e = 0; e < noun.length; e++) {
+        for (let s = 0; e < extension.length; s++) {
+          console.log(pronoun[i] + adj[t] + noun[e] + extension[s]);
+        }
+      }
+    }
+  }
 };
